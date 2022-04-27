851cdd889f42285825a6bc028b36e3cd996b0f96
varshaashree6@gmail.com <varshaashree7@gmail.com>
2022-04-27 16:13:00 -0400

    Angular page navigation

--- Student_Form/src/app/app.component.ts
+++ Student_Form/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Student_Form';
+
+  constructor(private router: Router) {
+    this.router.navigate(['/', 'home']);
+  }
+
+  ngOnInit(){
+    
+  }
 }
+
